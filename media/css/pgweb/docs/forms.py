8a46587cf4eb041a2a45cc807cab818e689db359
Stephen Frost <sfrost@snowman.net>
2017-12-14 15:24:18 -0500

    remove extra pgweb dir...

--- media/css/pgweb/docs/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class DocCommentForm(forms.Form):
-	name = forms.CharField(max_length=100, required=True)
-	email = forms.EmailField(max_length=100, required=True)
-	shortdesc = forms.CharField(max_length=100, required=True,
-								label="Short description")
-	details = forms.CharField(required=True, widget=forms.Textarea)
