c81b637ceee3e0e9a84c3d1c8fac513ab31c4c17
Jonathan S. Katz <jonathan.katz@excoventures.com>
2018-04-05 09:33:42 -0400

    Optimizations to current generic form styles

--- pgweb/core/templatetags/pgfilters.py
+++ pgweb/core/templatetags/pgfilters.py
@@ -1,13 +1,31 @@
-from django.template.defaultfilters import stringfilter
 from django import template
+from django.template.defaultfilters import stringfilter
+
 register = template.Library()
+
+@register.filter(name='class_name')
+def class_name(ob):
+	return ob.__class__.__name__
+
+@register.filter(is_safe=True)
+def field_class(value, arg):
+	return value.as_widget(attrs={"class": arg})
 
 @register.filter(name='hidemail')
 @stringfilter
 def hidemail(value):
 	return value.replace('@', ' at ')
 
-@register.filter(name='class_name')
-def class_name(ob):
-	return ob.__class__.__name__
+@register.filter(is_safe=True)
+def ischeckbox(obj):
+	return obj.field.widget.__class__.__name__ in ["CheckboxInput", "CheckboxSelectMultiple"] and not getattr(obj.field, 'regular_field', False)
 
+@register.filter(is_safe=True)
+def isrequired_error(obj):
+    if obj.errors and obj.errors[0] == u"This field is required.":
+        return True
+    return False
+
+@register.filter(is_safe=True)
+def label_class(value, arg):
+	return value.label_tag(attrs={'class': arg})
