d02967858eb23fde1d006c41f3f0e502c8bbd969
Jonathan S. Katz <jonathan.katz@excoventures.com>
2018-04-04 23:24:16 -0400

    Update language on doc comment page

--- pgweb/docs/forms.py
+++ pgweb/docs/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 
 class DocCommentForm(forms.Form):
-	name = forms.CharField(max_length=100, required=True)
-	email = forms.EmailField(max_length=100, required=True)
-	shortdesc = forms.CharField(max_length=100, required=True,
-								label="Short description")
-	details = forms.CharField(required=True, widget=forms.Textarea)
+	name = forms.CharField(
+		label='Your Name',
+		max_length=100,
+		required=True)
+	email = forms.EmailField(
+		label='Your Email',
+		max_length=100,
+		required=True)
+	shortdesc = forms.CharField(
+		max_length=100,
+		required=True,
+		label="Subject",)
+	details = forms.CharField(
+		label='Please explain why you are commenting on this page',
+		required=True,
+		widget=forms.Textarea)
